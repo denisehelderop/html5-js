1add5df8cf40eb911aef529764a681e935b81e25
Elmer Bulthuis <elmerbulthuis@gmail.com>
2014-05-23 07:25:22 +0200

    tellers

---
+++ tellers/teller.js
@@ -0,0 +1,18 @@
+
+var teller1 = 0;
+function tel1(elementId){
+	/*
+	 * teller1 wordt opgehoogd en daarna wordt de waarde
+	 * van teller1 in element met is elementId gezet.
+	 */
+
+}
+
+var teller2 = 0;
+function tel2(elementId){
+	/*
+	 * zelfde alleen dan met teller2
+	 */
+}
+
+
---
+++ tellers/tellerPro.js
@@ -0,0 +1,15 @@
+
+/*
+ * http://www.w3schools.com/js/js_arrays.asp
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+var tellers = [0, 0];
+function tel(elementId, tellerNummer){
+	/*
+	 * teller[tellerNummer] wordt opgehoogd en daarna wordt de waarde
+	 * van teller[tellerNummer] in element met is elementId gezet.
+	 */
+
+}
+
+
