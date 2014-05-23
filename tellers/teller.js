cbf10b64d4771d7066bea07a68b9b3c10d79c459
Denise Helderop <denise.helderop@gmail.com>
2014-05-23 17:22:56 +0200

    got it

--- tellers/teller.js
+++ tellers/teller.js
@@ -1,18 +1,12 @@
-
-var teller1 = 0;
-function tel1(elementId){
-	/*
-	 * teller1 wordt opgehoogd en daarna wordt de waarde
-	 * van teller1 in element met is elementId gezet.
-	 */
-
+var teller1 = 0
+function tel1(elementId)
+{
+		teller1 += 1;
+		document.getElementById(elementId).innerHTML = teller1;
 }
-
-var teller2 = 0;
-function tel2(elementId){
-	/*
-	 * zelfde alleen dan met teller2
-	 */
+	var teller2 = 0;
+function tel2(elementId)
+{
+		teller2 += 1;
+		document.getElementById(elementId).innerHTML = teller2;
 }
-
-
