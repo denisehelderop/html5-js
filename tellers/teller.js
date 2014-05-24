0b595acd07905ba97fee58e0389fb3a1bb29298f
Denise Helderop <denise.helderop@gmail.com>
2014-05-24 23:17:37 +0200

    Merge branch 'master'
    Conflicts:
    tellers/tellerPro.js

--- tellers/teller.js
+++ tellers/teller.js
@@ -1,10 +1,11 @@
-var teller1 = 0
+var teller1 = 0;
 function tel1(elementId)
 {
 		teller1 += 1;
 		document.getElementById(elementId).innerHTML = teller1;
 }
-	var teller2 = 0;
+
+var teller2 = 0;
 function tel2(elementId)
 {
 		teller2 += 1;
