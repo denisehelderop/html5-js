f9bb133fb8a6c9188ccbd7c000c919fe333ba5ce
Denise Helderop <denise.helderop@gmail.com>
2014-05-24 23:15:23 +0200

    array

--- tellers/teller.js
+++ tellers/teller.js
@@ -10,3 +10,5 @@ function tel2(elementId)
 		teller2 += 1;
 		document.getElementById(elementId).innerHTML = teller2;
 }
+//alle functies uitschrijven dan hoef je niets speciaals te doen. als je dit niet wilt omdat het veel tijd kost en ze 
+//in principe allemaal hetzelfde doen kun je Arrays gebruiken. Zie opdracht tellerPro
--- tellers/tellerPro.js
+++ tellers/tellerPro.js
@@ -3,13 +3,15 @@
  * http://www.w3schools.com/js/js_arrays.asp
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
  */
-var tellers = [0, 0];
-function tel(elementId, tellerNummer){
+var tellers = [0, 0, 0]; /*tussen gekke haakjes staat een array*/
+function tel(elementId, tellerNummer, optellen){
 	/*
-	 * teller[tellerNummer] wordt opgehoogd en daarna wordt de waarde
-	 * van teller[tellerNummer] in element met is elementId gezet.
+	 * tellers[tellerNummer] wordt opgehoogd en daarna wordt de waarde
+	 * van tellers[tellerNummer] in element met elementId gezet.
 	 */
+		tellers[tellerNummer] = tellers[tellerNummer] + optellen;
+		document.getElementById(elementId).innerHTML = tellers[tellerNummer];
 
 }
-
+/*JavaScript arrays are used to store multiple values in a single variable.*/
 
