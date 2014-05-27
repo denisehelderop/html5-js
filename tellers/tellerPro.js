593dcb6c1a27ba74b3f32c78960970b525117598
Denise Helderop <denise.helderop@gmail.com>
2014-05-27 06:45:51 +0200

    opdracht

--- tellers/tellerPro.js
+++ tellers/tellerPro.js
@@ -4,13 +4,17 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
  */
 var tellers = [0, 0, 0]; /*tussen gekke haakjes staat een array*/
+var tekstjes = [
+	"aap", "noot", "mies",
+	"wim", "jet", "oink"
+];
 function tel(elementId, tellerNummer, optellen){
 	/*
 	 * tellers[tellerNummer] wordt opgehoogd en daarna wordt de waarde
 	 * van tellers[tellerNummer] in element met elementId gezet.
 	 */
-		tellers[tellerNummer] = tellers[tellerNummer] + optellen;
-		document.getElementById(elementId).innerHTML = tellers[tellerNummer];
+		tellers[tellerNummer] = (tellers[tellerNummer] + optellen) % tekstjes.length;
+		document.getElementById(elementId).innerHTML = tekstjes[tellers[tellerNummer]];
 
 }
 /*JavaScript arrays are used to store multiple values in a single variable.*/
